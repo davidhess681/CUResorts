a43fbb5a18df9a10cc7fc33e9ee5c0366c4c8466
david <david.hess681@gmail.com>
2019-01-28 15:29:57 -0500

    First commit

---
+++ CUResorts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CUResorts.MvcApplication" Language="C#" %>
---
+++ CUResorts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CUResorts.Startup))]
+namespace CUResorts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
